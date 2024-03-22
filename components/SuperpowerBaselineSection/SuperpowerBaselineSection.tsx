78fc74c3b84b0f848ab4bf6fd5e7cf5d975aff35
Alvin Leung <alvinleung2009@gmail.com>
2024-03-22 07:39:56 -0700

    Add margin to typography

--- components/SuperpowerBaselineSection/SuperpowerBaselineSection.tsx
+++ components/SuperpowerBaselineSection/SuperpowerBaselineSection.tsx
@@ -64,11 +64,11 @@ function SuperpowerBaselineSection({}: Props) {
 
   return (
     <section className="flex flex-col items-center">
-      <div className="font-mono-sm mb-4 text-center">How it works</div>
-      <h2 className="font-sans-4xl mb-6 max-w-[20ch] text-center">
+      <div className="font-mono-sm mx-4 mb-4 text-center">How it works</div>
+      <h2 className="font-sans-4xl mx-4 mb-6 max-w-[20ch] text-center">
         Our cornerstone is the superpower baseline
       </h2>
-      <p className="font-sans-lg max-w-[50ch] text-center opacity-50">
+      <p className="font-sans-lg mx-4 max-w-[50ch] text-center opacity-50">
         Membership includes unlimited messaging with your doctor team, a
         comprehensive annual assessment with 80 advanced lab tests, and a custom
         action plan to improve your health and transform your life.
