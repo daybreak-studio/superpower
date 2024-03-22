adeed5eb60503a8c4c881c5464025ab1980a268a
Alvin Leung <alvinleung2009@gmail.com>
2024-03-21 22:30:29 -0700

    Refine carousel indicator

--- components/SuperpowerBaselineSection/BaselineCarousel.tsx
+++ components/SuperpowerBaselineSection/BaselineCarousel.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { SlideInfo } from "./SuperpowerBaselineSection";
 import Carousel, { useCarouselControls } from "../Carousel/Carousel";
 import BaselineCarouselItem from "./BaselineCarouselItem";
+import { motion } from "framer-motion";
+import { useBoundingClientRect } from "@/hooks/useBoundingClientRect";
+import { AnimationConfig } from "../AnimationConfig";
+import { CarouselPageIndicator } from "./CarouselPageIndicator";
 
 type Props = {
   slides: Array<SlideInfo>;
 };
 
 const BaselineCarousel = ({ slides }: Props) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const controls = useCarouselControls();
   const handleNextClick = () => {
     controls.next();
@@ -19,14 +25,17 @@ const BaselineCarousel = ({ slides }: Props) => {
   };
 
   return (
-    <div className="w-full pt-12">
-      <Carousel controls={controls}>
+    <div className="flex w-full flex-col pt-12">
+      <Carousel controls={controls} onSlideChange={setCurrentSlide}>
         {slides.map((slide, index) => (
           <BaselineCarouselItem key={index} slide={slide} />
         ))}
       </Carousel>
-      <button onClick={handlePrevClick}>prev</button>
-      <button onClick={handleNextClick}>next</button>
+
+      <div className="mx-auto mt-8 flex flex-col items-center">
+        <div className="mb-4 h-8 w-[1px] bg-[#FC5F2B] opacity-40" />
+        <CarouselPageIndicator currentSlide={currentSlide} slides={slides} />
+      </div>
     </div>
   );
 };
