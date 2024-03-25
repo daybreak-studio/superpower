4ebfa240883ce2e42497481eb1526e704a163bf1
kiranpate1 <kp8568@gmail.com>
2024-03-25 16:14:55 -0400

    initialized testimonial page and main components

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,14 @@
 import ScrollVideo from "@/components/ScrollVideo/ScrollVideo";
 import HeroSection from "@/components/Sections/HeroSection/HeroSection";
 import SuperpowerBaselineSection from "@/components/Sections/SuperpowerBaselineSection/SuperpowerBaselineSection";
+import TestimonialsSection from "@/components/Sections/TestimonialsSection/TestimonialsWrapper"; // Import the missing component
+
 import { ReactLenis } from "@studio-freight/react-lenis";
 
 export default function Home() {
   return (
-    <main>
-      <ReactLenis root>
-        <HeroSection />
-        <SuperpowerBaselineSection />
-        <SuperpowerBaselineSection />
-      </ReactLenis>
-    </main>
+    <div>
+      <TestimonialsSection /> {/* Use the imported component */}
+    </div>
   );
 }
---
+++ components/Sections/TestimonialsSection/TestimonialsWrapper.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const TestimonialsWrapper = (props: Props) => {
+  return <div>{/* Your code here */}</div>;
+};
+
+export default TestimonialsWrapper;
