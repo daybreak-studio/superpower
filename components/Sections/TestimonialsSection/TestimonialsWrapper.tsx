2b3e9ad1db78cf7f67e210909d901b147c7c6d40
kiranpate1 <kp8568@gmail.com>
2024-03-25 18:07:38 -0400

    updates to component

--- components/Sections/TestimonialsSection/TestimonialsWrapper.tsx
+++ components/Sections/TestimonialsSection/TestimonialsWrapper.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import LineElement from "@/components/LineElement/LineElement";
 
 type Props = {};
 
 const TestimonialsWrapper = (props: Props) => {
-  return <div>{/* Your code here */}</div>;
+  return (
+    <div>
+      <div className="absolute mt-12 flex h-4">
+        <LineElement length={100} color={"rgba(0,0,0,.5)"} vertical tail={32} />
+      </div>
+
+      <div className="my-24 max-w-[24ch] text-center">
+        <p className="font-sans-xl mb-3 leading-tight">
+          The world's most advanced digital clinic for prevention, performance
+          and longevity.
+        </p>
+        <p className="font-sans-xl mb-3 leading-tight">
+          The world's most advanced digital clinic for prevention, performance
+          and longevity.
+        </p>
+        <p className="font-sans-xl mb-3 leading-tight">
+          The world's most advanced digital clinic for prevention, performance
+          and longevity.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default TestimonialsWrapper;
