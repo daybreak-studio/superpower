50dd77c9b560d1d978ca9d6a50492ab5834f85df
kiranpate1 <kp8568@gmail.com>
2024-03-28 16:12:45 -0400

    Begun adding styling for section

---
+++ components/Sections/TestimonialsSection/TestimonialsSelector.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const TestimonialsSelector = (props: Props) => {
+  return (
+    <div>
+      <div className="border-solid border border-[#bbb] h-3 w-3" />
+    </div>
+  );
+};
+
+export default TestimonialsSelector;
--- components/Sections/TestimonialsSection/TestimonialsWrapper.tsx
+++ components/Sections/TestimonialsSection/TestimonialsWrapper.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 import LineElement from "@/components/LineElement/LineElement";
+import TestimonialsSelector from "./TestimonialsSelector";
 
 type Props = {};
 
 const TestimonialsWrapper = (props: Props) => {
   return (
     <div>
-      <div className="absolute mt-12 flex h-4">
-        <LineElement length={100} color={"rgba(0,0,0,.5)"} vertical tail={32} />
-      </div>
+      <div className="absolute w-full flex flex-col gap-10 justify-center items-center">
+        <div className="flex h-8">
+          <LineElement length={100} color={"rgba(0,0,0,.5)"} vertical tail={16} />
+        </div>
 
-      <div className="my-24 max-w-[24ch] text-center">
-        <p className="font-sans-xl mb-3 leading-tight">The</p>
-        <p className="font-sans-xl mb-3 leading-tight">The</p>
-        <p className="font-sans-xl mb-3 leading-tight">The</p>
+        <div className="flex flex-col justify-between flex-wrap text-center h-[400px]">
+          <div className="flex gap-3">
+            <TestimonialsSelector />
+            <TestimonialsSelector />
+            <TestimonialsSelector />
+          </div>
+          <p className="font-sans-xl mb-3 leading-tight">The</p>
+          <p className="font-sans-xl mb-3 leading-tight">The</p>
+          <p className="font-sans-xl mb-3 leading-tight">The</p>
+        </div>
       </div>
     </div>
   );
