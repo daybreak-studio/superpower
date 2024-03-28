96624d9563398c9663b0eb9c465a7959bea68f2f
kiranpate1 <kp8568@gmail.com>
2024-03-28 16:57:20 -0400

    finish desktop+mobile look

--- components/Sections/TestimonialsSection/TestimonialsWrapper.tsx
+++ components/Sections/TestimonialsSection/TestimonialsWrapper.tsx
@@ -7,20 +7,31 @@ type Props = {};
 const TestimonialsWrapper = (props: Props) => {
   return (
     <div>
-      <div className="absolute w-full flex flex-col gap-10 justify-center items-center">
-        <div className="flex h-8">
-          <LineElement length={100} color={"rgba(0,0,0,.5)"} vertical tail={16} />
+      <div className="absolute w-full h-full flex flex-col gap-6 justify-center items-center">
+        <div className="flex h-full">
+          <LineElement length={"auto"} color={"rgba(0,0,0,.5)"} vertical tail={16} />
         </div>
 
-        <div className="flex flex-col justify-between flex-wrap text-center h-[400px]">
-          <div className="flex gap-3">
-            <TestimonialsSelector />
-            <TestimonialsSelector />
-            <TestimonialsSelector />
+        <div className="flex flex-row w-full justify-center items-center">
+          <div className="flex flex-row w-full">
+            <LineElement length={"auto"} color={"rgba(0,0,0,.5)"} horizontal tail={16} />
           </div>
-          <p className="font-sans-xl mb-3 leading-tight">The</p>
-          <p className="font-sans-xl mb-3 leading-tight">The</p>
-          <p className="font-sans-xl mb-3 leading-tight">The</p>
+          <div className="flex flex-col justify-between flex-wrap items-center text-center p-4 min-h-[250px] lg:min-h-[500px] min-w-96 lg:min-w-[624px]">
+            <div className="flex gap-3">
+              <TestimonialsSelector />
+              <TestimonialsSelector />
+              <TestimonialsSelector />
+            </div>
+            <p className="font-sans-4xl mx-4 mb-6 max-w-[20ch] text-center">“Changed my life forever.”</p>
+            <p className="font-mono-sm text-center">Vinay hiremath, Co-counder of loom</p>
+          </div>
+          <div className="flex flex-row w-full">
+            <LineElement length={"auto"} color={"rgba(0,0,0,.5)"} horizontal head={16} />
+          </div>
+        </div>
+
+        <div className="flex h-full">
+          <LineElement length={"auto"} color={"rgba(0,0,0,.5)"} vertical head={16} />
         </div>
       </div>
     </div>
