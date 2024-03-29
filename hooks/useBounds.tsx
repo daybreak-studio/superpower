83e3a784ff6f0e8548d954e8a0c2e0d28a9d1907
Alvin Leung <alvinleung2009@gmail.com>
2024-03-29 12:21:15 -0700

    Add usePointerInfo Hooks

--- hooks/useBounds.tsx
+++ hooks/useBounds.tsx
@@ -7,7 +7,7 @@ import {
   useState,
 } from "react";
 
-type BoundingBoxInfo = {
+export type BoundingBoxInfo = {
   x: number;
   y: number;
   width: number;
@@ -33,7 +33,7 @@ type BoundingBoxInfo = {
  * @returns [containerRef, bounds]
  */
 export function useBounds<T extends HTMLElement>(
-  dependency?: any[],
+  dependency: any[] = [],
 ): [MutableRefObject<T>, BoundingBoxInfo] {
   const containerRef = useRef<T>() as MutableRefObject<T>;
   const [bounds, setBounds] = useState({
@@ -72,4 +72,46 @@ export function useBounds<T extends HTMLElement>(
   }, dependency);
 
   return [containerRef, bounds];
+}
+
+export function useBoundsWithRef<T extends HTMLElement>(
+  containerRef: MutableRefObject<T>,
+  dependency: any[] = [],
+): BoundingBoxInfo {
+  const [bounds, setBounds] = useState({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  });
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      const bounds = containerRef.current.getBoundingClientRect();
+
+      const scrollOffset = window.scrollY;
+
+      setBounds({
+        x: bounds.x,
+        y: bounds.y + scrollOffset,
+        width: bounds.width,
+        height: bounds.height,
+        left: bounds.left,
+        right: bounds.right,
+        top: bounds.top + scrollOffset,
+        bottom: bounds.bottom + scrollOffset,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, dependency);
+
+  return bounds;
 }
