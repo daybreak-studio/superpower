eb5c878b8e13c9e6129ac9ff00faf65a3a82f3d6
kiranpate1 <kp8568@gmail.com>
2024-03-29 23:16:14 -0400

    Switched composition around

--- components/Sections/TestimonialsSection/TestimonialsDesktop.tsx
+++ components/Sections/TestimonialsSection/TestimonialsDesktop.tsx
@@ -1,107 +1,68 @@
 import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import LineElement from "@/components/LineElement/LineElement";
 import TestimonialsSelector from "./TestimonialsSelector";
 import Corner from "@/components/Button/Corner";
 
-const quotes = [
-  {
-    quote: "Changed my life forever.",
-    author: "Vinay hiremath, Co-founder of loom",
-  },
-  {
-    quote: "The best thing since sliced bread",
-    author: "Kiran Patel",
-  },
-  {
-    quote: "Hello there! This is a longer quote",
-    author: "discwdicnwis",
-  },
-];
+type props = {
+  quoteInfo: { quote: string; author: string }[];
+  quoteInfoIndex: number;
+};
 
-const TestimonialsDesktop = () => {
-  const [quoteIndex, setQuoteIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setQuoteIndex((prevIndex) => (prevIndex + 1) % quotes.length);
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, []);
-
+const TestimonialsDesktop = ({ quoteInfo, quoteInfoIndex }: props) => {
   return (
     <div>
-      <div className="absolute flex h-full w-full flex-col items-center justify-center gap-6">
-        <div className="flex h-full">
-          <LineElement length={"auto"} color={"#bbb"} vertical tail={16} />
-        </div>
-
-        <div className="flex w-full flex-row items-center justify-center gap-12">
-          <div className="flex grow-0 flex-row">
-            <LineElement length={"auto"} color={"#bbb"} horizontal tail={16} />
+      <AnimatePresence>
+        <motion.div
+          className="relative flex items-center justify-center overflow-hidden"
+          initial={{
+            maxWidth: 624,
+            height: 500,
+          }}
+          animate={{
+            maxWidth: [624, 0, 624],
+            height: [500, 0, 500],
+          }}
+          exit={{
+            maxWidth: 624,
+            height: 500,
+          }}
+          transition={{
+            duration: 0.6,
+            repeat: Infinity,
+            repeatDelay: 1.4,
+            delay: 1.7,
+          }}
+        >
+          <div className="absolute left-0 top-0">
+            <Corner topLeft size={20} />
           </div>
-          <AnimatePresence>
-            <motion.div
-              className="relative flex items-center justify-center overflow-hidden"
-              initial={{
-                maxWidth: 624,
-                height: 500,
-              }}
-              animate={{
-                maxWidth: [624, 0, 624],
-                height: [500, 0, 500],
-              }}
-              exit={{
-                maxWidth: 624,
-                height: 500,
-              }}
-              transition={{
-                duration: 0.6,
-                repeat: Infinity,
-                repeatDelay: 1.4,
-                delay: 1.7,
-              }}
-            >
-              <div className="absolute left-0 top-0">
-                <Corner topLeft size={20} />
-              </div>
-              <div className="absolute right-0 top-0">
-                <Corner topRight size={20} />
-              </div>
-              <div className="absolute bottom-0 left-0">
-                <Corner bottomLeft size={20} />
-              </div>
-              <div className="absolute bottom-0 right-0">
-                <Corner bottomRight size={20} />
-              </div>
-              <div className="flex min-h-[500px] min-w-[624px] flex-col flex-wrap items-center justify-between p-4 text-center">
-                <div className="flex gap-3">
-                  {quotes.map((quote, index) => (
-                    <TestimonialsSelector
-                      key={index}
-                      active={index === quoteIndex}
-                    />
-                  ))}
-                </div>
-                <p className="font-sans-4xl mx-4 mb-6 max-w-[20ch] text-center">
-                  {quotes[quoteIndex].quote}
-                </p>
-                <p className="font-mono-sm text-center">
-                  {quotes[quoteIndex].author}
-                </p>
-              </div>
-            </motion.div>
-          </AnimatePresence>
-          <div className="flex grow-0 flex-row">
-            <LineElement length={"auto"} color={"#bbb"} horizontal head={16} />
+          <div className="absolute right-0 top-0">
+            <Corner topRight size={20} />
           </div>
-        </div>
-
-        <div className="flex h-full">
-          <LineElement length={"auto"} color={"#bbb"} vertical head={16} />
-        </div>
-      </div>
+          <div className="absolute bottom-0 left-0">
+            <Corner bottomLeft size={20} />
+          </div>
+          <div className="absolute bottom-0 right-0">
+            <Corner bottomRight size={20} />
+          </div>
+          <div className="flex min-h-[500px] min-w-[624px] flex-col flex-wrap items-center justify-between p-4 text-center">
+            <div className="flex gap-3">
+              {quoteInfo.map((quote, index) => (
+                <TestimonialsSelector
+                  key={index}
+                  active={index === quoteInfoIndex}
+                />
+              ))}
+            </div>
+            <p className="font-sans-4xl mx-4 mb-6 max-w-[20ch] text-center">
+              {quoteInfo[quoteInfoIndex].quote}
+            </p>
+            <p className="font-mono-sm text-center">
+              {quoteInfo[quoteInfoIndex].author}
+            </p>
+          </div>
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
--- components/Sections/TestimonialsSection/TestimonialsWrapper.tsx
+++ components/Sections/TestimonialsSection/TestimonialsWrapper.tsx
@@ -2,14 +2,68 @@ import React, { useState, useEffect } from "react";
 import { breakpoints, useBreakpoint } from "@/hooks/useBreakpoints";
 import TestimonialsDesktop from "./TestimonialsDesktop";
 import TestimonialsMobile from "./TestimonialsMobile";
+import LineElement from "@/components/LineElement/LineElement";
+
+const quotes = [
+  {
+    quote: "Changed my life forever.",
+    author: "Vinay hiremath, Co-founder of loom",
+  },
+  {
+    quote: "The best thing since sliced bread",
+    author: "Kiran Patel",
+  },
+  {
+    quote: "Hello there! This is a longer quote",
+    author: "discwdicnwis",
+  },
+];
 
 const TestimonialsWrapper = () => {
   const isDesktop = useBreakpoint(breakpoints.desktop);
+  const [quoteIndex, setQuoteIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setQuoteIndex((prevIndex) => (prevIndex + 1) % quotes.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <section>
-      {isDesktop && <TestimonialsDesktop />}
-      {!isDesktop && <TestimonialsMobile />}
+      <div className="absolute flex h-full w-full flex-col items-center justify-center gap-6">
+        <div className="flex h-full">
+          <LineElement length={"auto"} color={"#bbb"} vertical tail={16} />
+        </div>
+
+        <div className="flex w-full flex-row items-center justify-center gap-12">
+          <div className="flex grow-0 flex-row">
+            <LineElement length={"auto"} color={"#bbb"} horizontal tail={16} />
+          </div>
+
+          {isDesktop && (
+            <TestimonialsDesktop
+              quoteInfo={quotes}
+              quoteInfoIndex={quoteIndex}
+            />
+          )}
+          {!isDesktop && (
+            <TestimonialsMobile
+              quoteInfo={quotes}
+              quoteInfoIndex={quoteIndex}
+            />
+          )}
+          <div className="flex grow-0 flex-row">
+            <LineElement length={"auto"} color={"#bbb"} horizontal head={16} />
+          </div>
+        </div>
+
+        <div className="flex h-full">
+          <LineElement length={"auto"} color={"#bbb"} vertical head={16} />
+        </div>
+      </div>
     </section>
   );
 };
