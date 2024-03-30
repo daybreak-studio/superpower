19e62b4e1e51cc756b132609140a37809af7c0a0
kiranpate1 <kp8568@gmail.com>
2024-03-29 21:39:46 -0400

    Added corner styling

--- components/Sections/TestimonialsSection/TestimonialsWrapper.tsx
+++ components/Sections/TestimonialsSection/TestimonialsWrapper.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import LineElement from "@/components/LineElement/LineElement";
 import TestimonialsSelector from "./TestimonialsSelector";
+import Corner from "@/components/Button/Corner";
 
 const quotes = [
   {
@@ -42,7 +43,7 @@ const TestimonialsWrapper = () => {
           </div>
           <AnimatePresence>
             <motion.div
-              className="flex h-full w-full items-center justify-center overflow-hidden"
+              className="relative flex h-full w-full items-center justify-center overflow-hidden"
               initial={{
                 maxWidth: 624,
                 maxHeight: 500,
@@ -62,6 +63,18 @@ const TestimonialsWrapper = () => {
                 delay: 1.7,
               }}
             >
+              <div className="absolute left-0 top-0">
+                <Corner topLeft size={20} />
+              </div>
+              <div className="absolute right-0 top-0">
+                <Corner topRight size={20} />
+              </div>
+              <div className="absolute bottom-0 left-0">
+                <Corner bottomLeft size={20} />
+              </div>
+              <div className="absolute bottom-0 right-0">
+                <Corner bottomRight size={20} />
+              </div>
               <div className="flex min-h-[250px] min-w-96 flex-col flex-wrap items-center justify-between p-4 text-center lg:min-h-[500px] lg:min-w-[624px]">
                 <div className="flex gap-3">
                   {quotes.map((quote, index) => (
