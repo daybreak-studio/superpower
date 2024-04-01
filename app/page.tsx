e85ee1ad642e480371fca218ff2b32847a0afb2c
kiranpate1 <kp8568@gmail.com>
2024-04-01 16:50:57 -0400

    test

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,6 @@ export default function Home() {
   return (
     <div>
       <TestimonialsSection /> {/* Use the imported component */}
-      <TestimonialsSection /> {/* Use the imported component */}
-      <TestimonialsSection /> {/* Use the imported component */}
     </div>
   );
 }
