59c63795188630d4947e280e3b78348193ef497c
kiranpate1 <kp8568@gmail.com>
2024-04-10 16:47:04 -0400

    added transition images

--- components/Sections/TransitionSection/TransitionSectionWrapper.tsx
+++ components/Sections/TransitionSection/TransitionSectionWrapper.tsx
@@ -3,10 +3,37 @@ import { breakpoints, useBreakpoint } from "@/hooks/useBreakpoints";
 
 type Props = {};
 
-const Testimonial = (props: Props) => {
+const TransitionSectionWrapper = (props: Props) => {
   const isDesktop = useBreakpoint(breakpoints.lg);
 
-  return <div className="flex flex-col gap-6"></div>;
+  return (
+    <section className="flex h-0 w-full items-end">
+      <div className="flex h-screen w-screen">
+        <div className="flex flex-col justify-center gap-6">
+          <div className="flex h-full">
+            <div className="h-1 w-1 bg-black"></div>
+          </div>
+
+          <div className="flex w-full flex-row items-center justify-center gap-12">
+            <div className="flex w-full flex-row">
+              <div className="h-1 w-1 bg-black"></div>
+            </div>
+
+            <div>
+              <div className="h-1 w-1 bg-black"></div>
+            </div>
+            <div className="flex w-full flex-row">
+              <div className="h-1 w-1 bg-black"></div>
+            </div>
+          </div>
+
+          <div className="flex h-full">
+            <div className="h-1 w-1 bg-black"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default Testimonial;
+export default TransitionSectionWrapper;
