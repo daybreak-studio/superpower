6b43fc6eb6575915294056de6b36cd829e71059d
kiranpate1 <kp8568@gmail.com>
2024-04-10 18:28:57 -0400

    updated images/added animation

--- components/Sections/TransitionSection/TransitionSectionWrapper.tsx
+++ components/Sections/TransitionSection/TransitionSectionWrapper.tsx
@@ -1,35 +1,68 @@
-import React from "react";
+import React, { useState, useEffect, MutableRefObject, useRef } from "react";
+import { motion, useInView } from "framer-motion";
+import Image from "next/image";
 import { breakpoints, useBreakpoint } from "@/hooks/useBreakpoints";
 
 type Props = {};
 
 const TransitionSectionWrapper = (props: Props) => {
   const isDesktop = useBreakpoint(breakpoints.lg);
+  const [scrollProgress, setScrollProgress] = useState(0);
+
+  const handleScroll = () => {
+    const section = document.querySelector(
+      ".relative.h-screen.w-screen.pointer-events-none",
+    ) as HTMLElement;
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionBottom = sectionTop + sectionHeight;
+
+    const scrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const viewportCenter = windowHeight / 4;
+
+    if (
+      scrollPosition >= sectionTop &&
+      scrollPosition <= sectionBottom - viewportCenter
+    ) {
+      const scrollProgress =
+        ((scrollPosition - sectionTop) /
+          (sectionHeight - windowHeight + viewportCenter)) *
+        100;
+      setScrollProgress(scrollProgress);
+    } else if (scrollPosition < sectionTop - viewportCenter) {
+      setScrollProgress(0);
+    } else if (scrollPosition > sectionBottom - viewportCenter) {
+      setScrollProgress(200);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const easedScrollProgress = ease(scrollProgress);
+
+  function ease(t: number) {
+    return t * t;
+  }
 
   return (
     <section className="flex h-0 w-full items-end">
-      <div className="flex h-screen w-screen">
-        <div className="flex flex-col justify-center gap-6">
-          <div className="flex h-full">
-            <div className="h-1 w-1 bg-black"></div>
-          </div>
-
-          <div className="flex w-full flex-row items-center justify-center gap-12">
-            <div className="flex w-full flex-row">
-              <div className="h-1 w-1 bg-black"></div>
-            </div>
-
-            <div>
-              <div className="h-1 w-1 bg-black"></div>
-            </div>
-            <div className="flex w-full flex-row">
-              <div className="h-1 w-1 bg-black"></div>
-            </div>
-          </div>
-
-          <div className="flex h-full">
-            <div className="h-1 w-1 bg-black"></div>
-          </div>
+      <div className="pointer-events-none relative h-screen w-screen">
+        <div className="absolute bottom-0 left-0 h-auto w-full">
+          <Image
+            src="/transition-section/bottom.png"
+            width="0"
+            height="0"
+            sizes="100vw"
+            className="w-full"
+            style={{
+              height: `${(scrollProgress * window.innerHeight) / 200}px`,
+            }} // Adjust the height based on scrollProgress
+            alt="transition-bg"
+          />
         </div>
       </div>
     </section>
