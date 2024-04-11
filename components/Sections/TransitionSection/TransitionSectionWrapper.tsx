ef834c40386d6c3f1412a5e1078a0416626054c3
kiranpate1 <kp8568@gmail.com>
2024-04-11 19:49:43 -0400

    Updated animation

--- components/Sections/TransitionSection/TransitionSectionWrapper.tsx
+++ components/Sections/TransitionSection/TransitionSectionWrapper.tsx
@@ -1,5 +1,22 @@
-import React, { useState, useEffect, MutableRefObject, useRef } from "react";
-import { motion, useInView } from "framer-motion";
+import React, {
+  useState,
+  useEffect,
+  MutableRefObject,
+  useRef,
+  RefObject,
+} from "react";
+import {
+  cubicBezier,
+  motion,
+  useInView,
+  useScroll,
+  useTransform,
+  useSpring,
+} from "framer-motion";
+import {
+  usePointerOffset,
+  usePointerOffsetNormalized,
+} from "@/hooks/usePointerInfo";
 import Image from "next/image";
 import { breakpoints, useBreakpoint } from "@/hooks/useBreakpoints";
 
@@ -7,84 +24,67 @@ type Props = {};
 
 const TransitionSectionWrapper = (props: Props) => {
   const isDesktop = useBreakpoint(breakpoints.lg);
-  const [scrollProgress, setScrollProgress] = useState(0);
 
-  const handleScroll = () => {
-    const section = document.querySelector(
-      ".relative.h-screen.w-screen.pointer-events-none",
-    ) as HTMLElement;
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.offsetHeight;
-    const sectionBottom = sectionTop + sectionHeight;
+  const fuckthisshit = useRef() as MutableRefObject<HTMLAnchorElement>;
 
-    const scrollPosition = window.scrollY;
-    const windowHeight = window.innerHeight;
-    const viewportCenter = windowHeight / 4;
+  const inViewDelay = 0.3;
+  const [isHovering, setIsHovering] = useState(false);
 
-    if (
-      scrollPosition >= sectionTop &&
-      scrollPosition <= sectionBottom - viewportCenter
-    ) {
-      const scrollProgress =
-        ((scrollPosition - sectionTop) /
-          (sectionHeight - windowHeight + viewportCenter)) *
-        100;
-      setScrollProgress(scrollProgress);
-    } else if (scrollPosition < sectionTop - viewportCenter) {
-      setScrollProgress(0);
-    } else if (scrollPosition > sectionBottom - viewportCenter) {
-      // setScrollProgress(200);
-    }
-  };
+  const pointerOffset = usePointerOffset(isHovering, fuckthisshit, "center");
+  const pointerOffsetPercent = usePointerOffsetNormalized(pointerOffset);
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  const offsetX = useTransform(pointerOffsetPercent.x, (latest) =>
+    isHovering ? latest * 15 : 0,
+  );
+  const offsetY = useTransform(pointerOffsetPercent.y, (latest) =>
+    isHovering ? latest * 4 : 0,
+  );
 
-  const easedScrollProgress = ease(scrollProgress);
+  const textX = useSpring(offsetX, { stiffness: 300, damping: 50 });
+  const textY = useSpring(offsetY, { stiffness: 300, damping: 50 });
 
-  function ease(t: number) {
-    return t * t;
-  }
+  const containerRef = useRef() as RefObject<HTMLElement>;
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    // offset: ["start end", "end end"],
+  });
+
+  const easedScroll = useTransform(scrollYProgress, (t) => {
+    var ts = t * t;
+    return (1 - ts) * 5;
+  });
+  // const easedScroll = useTransform(scrollYProgress, [0, 1], [0.5, 1], {
+  //   ease: cubicBezier(0.16, 1, 0.3, 1),
+  // });
 
   return (
-    <section className="flex h-0 w-full items-end">
-      <div className="pointer-events-none relative h-screen w-screen">
+    <section ref={containerRef} className="flex h-0 w-full items-end">
+      <div
+        ref={fuckthisshit as unknown as MutableRefObject<HTMLDivElement>}
+        className="pointer-events-none relative h-screen w-screen"
+      >
         <div className="absolute bottom-0 left-0 h-auto w-full">
-          <Image
-            src="/transition-section/top.png"
-            width="0"
-            height="0"
-            sizes="100vw"
-            className="absolute bottom-0 left-0 w-full mix-blend-hard-light"
-            style={{
-              height: `${(scrollProgress * window.innerHeight) / 200}px`,
-            }} // Adjust the height based on scrollProgress
-            alt="transition-bg"
-          />
-          <Image
-            src="/transition-section/mid.png"
-            width="0"
-            height="0"
-            sizes="100vw"
-            className="absolute bottom-0 left-0 w-full mix-blend-hard-light"
-            style={{
-              height: `${(scrollProgress * window.innerHeight) / 200}px`,
-            }} // Adjust the height based on scrollProgress
-            alt="transition-bg"
-          />
-          <Image
-            src="/transition-section/bot.png"
-            width="0"
-            height="0"
-            sizes="100vw"
-            className="absolute bottom-0 left-0 w-full mix-blend-soft-light"
-            style={{
-              height: `${(scrollProgress * window.innerHeight) / 200}px`,
-            }} // Adjust the height based on scrollProgress
-            alt="transition-bg"
-          />
+          <motion.div
+            className="flex origin-bottom flex-row  mix-blend-hard-light"
+            style={{ scaleY: easedScroll }}
+          >
+            <Image
+              src="/transition-section/left.png"
+              width="0"
+              height="0"
+              sizes="100vw"
+              className="grow"
+              alt="transition-bg"
+            />
+            <Image
+              src="/transition-section/right.png"
+              width="0"
+              height="0"
+              sizes="100vw"
+              className="grow"
+              alt="transition-bg"
+            />
+          </motion.div>
         </div>
       </div>
     </section>
