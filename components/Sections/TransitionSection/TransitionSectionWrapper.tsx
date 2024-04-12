d219636cb6e5f451d20779c6a7a7df9e6a12aa3a
kiranpate1 <kp8568@gmail.com>
2024-04-12 19:37:38 -0400

    Updated animations

--- components/Sections/TransitionSection/TransitionSectionWrapper.tsx
+++ components/Sections/TransitionSection/TransitionSectionWrapper.tsx
@@ -25,66 +25,80 @@ type Props = {};
 const TransitionSectionWrapper = (props: Props) => {
   const isDesktop = useBreakpoint(breakpoints.lg);
 
-  const fuckthisshit = useRef() as MutableRefObject<HTMLAnchorElement>;
+  const containerRef = useRef() as MutableRefObject<HTMLAnchorElement>;
 
-  const inViewDelay = 0.3;
   const [isHovering, setIsHovering] = useState(false);
 
-  const pointerOffset = usePointerOffset(isHovering, fuckthisshit, "center");
+  const pointerOffset = usePointerOffset(isHovering, containerRef, "center");
   const pointerOffsetPercent = usePointerOffsetNormalized(pointerOffset);
 
-  const offsetX = useTransform(pointerOffsetPercent.x, (latest) =>
-    isHovering ? latest * 15 : 0,
+  const offsetX1 = useTransform(pointerOffsetPercent.x, (latest) =>
+    isHovering ? 1 + latest : 1,
+  );
+  const offsetX2 = useTransform(pointerOffsetPercent.x, (latest) =>
+    isHovering ? 1 - latest : 1,
   );
   const offsetY = useTransform(pointerOffsetPercent.y, (latest) =>
-    isHovering ? latest * 4 : 0,
+    isHovering ? 1 - 0.75 * latest : 1,
   );
 
-  const textX = useSpring(offsetX, { stiffness: 300, damping: 50 });
-  const textY = useSpring(offsetY, { stiffness: 300, damping: 50 });
+  const easedX1 = useSpring(offsetX1, { stiffness: 400, damping: 100 });
+  const easedX2 = useSpring(offsetX2, { stiffness: 400, damping: 100 });
+  const easedY = useSpring(offsetY, { stiffness: 400, damping: 100 });
 
-  const containerRef = useRef() as RefObject<HTMLElement>;
   const { scrollYProgress } = useScroll({
     target: containerRef,
     // offset: ["start end", "end end"],
   });
 
   const easedScroll = useTransform(scrollYProgress, (t) => {
-    var ts = t * t;
-    return (1 - ts) * 5;
+    var ts = Math.pow(0.75 * t, 3);
+    return ts * 18;
   });
   // const easedScroll = useTransform(scrollYProgress, [0, 1], [0.5, 1], {
   //   ease: cubicBezier(0.16, 1, 0.3, 1),
   // });
 
   return (
-    <section ref={containerRef} className="flex h-0 w-full items-end">
+    <section className="flex h-[75vh] w-full items-end">
       <div
-        ref={fuckthisshit as unknown as MutableRefObject<HTMLDivElement>}
-        className="pointer-events-none relative h-screen w-screen"
+        onPointerEnter={() => setIsHovering(true)}
+        onPointerLeave={() => setIsHovering(false)}
+        ref={containerRef as unknown as MutableRefObject<HTMLDivElement>}
+        className="relative h-[225vh] w-screen"
       >
-        <div className="absolute bottom-0 left-0 h-auto w-full">
-          <motion.div
-            className="flex origin-bottom flex-row  mix-blend-hard-light"
-            style={{ scaleY: easedScroll }}
-          >
-            <Image
-              src="/transition-section/left.png"
-              width="0"
-              height="0"
-              sizes="100vw"
-              className="grow"
-              alt="transition-bg"
-            />
-            <Image
-              src="/transition-section/right.png"
-              width="0"
-              height="0"
-              sizes="100vw"
-              className="grow"
-              alt="transition-bg"
-            />
-          </motion.div>
+        <div className="sticky top-0 flex h-screen w-full items-end mix-blend-hard-light">
+          <div className="absolute bottom-0 left-0 h-auto w-full">
+            <motion.div
+              className="flex origin-bottom flex-row"
+              style={{ scaleY: easedScroll }}
+            >
+              <motion.img
+                src="/transition-section/left.png"
+                width="0"
+                height="0"
+                sizes="100vw"
+                className="grow origin-bottom-left"
+                alt="transition-bg"
+                style={{
+                  scaleX: easedX1,
+                  scaleY: easedY,
+                }}
+              />
+              <motion.img
+                src="/transition-section/right.png"
+                width="0"
+                height="0"
+                sizes="100vw"
+                className="grow origin-bottom-right"
+                alt="transition-bg"
+                style={{
+                  scaleX: easedX2,
+                  scaleY: easedY,
+                }}
+              />
+            </motion.div>
+          </div>
         </div>
       </div>
     </section>
