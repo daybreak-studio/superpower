0ebad353e74f2b18b35c4d6f294d2ba086fb0fc0
Alvin Leung <alvinleung2009@gmail.com>
2024-04-16 15:51:22 -0400

    Testimonials and biomarker bug fix

--- components/Sections/ProtocolSection/Biomarkers/BiomarkerBanner.tsx
+++ components/Sections/ProtocolSection/Biomarkers/BiomarkerBanner.tsx
@@ -1,4 +1,6 @@
-import React, { MutableRefObject, useRef } from "react";
+"use client";
+
+import React, { MutableRefObject, useMemo, useRef } from "react";
 import ProtocolSectionDesktop from "../desktop/ProtocolSectionDesktop";
 import { PROTOCOLS } from "../Protocols";
 import { breakpoints, useBreakpoint } from "@/hooks/useBreakpoints";
@@ -10,6 +12,8 @@ import { motion, useInView } from "framer-motion";
 import { BIOMARKERS } from "./Biomarkers";
 import { list } from "postcss";
 import { AnimationConfig } from "@/components/AnimationConfig";
+import { useRandom } from "@/hooks/useRandom";
+import { SimplexNoise } from "three/examples/jsm/math/SimplexNoise.js";
 
 type Props = {
   isVisible?: boolean;
@@ -27,11 +31,22 @@ const colors = [
 ];
 
 const BiomarkerBanner = ({ isVisible = true }: Props) => {
-  const shuffledList = BIOMARKERS.sort(() => Math.random() - 0.5);
-  const list1 = shuffledList.slice(0, 6);
-  const list2 = shuffledList.slice(6, shuffledList.length);
+  const random = useRandom([isVisible]);
+  const shuffledList = useMemo(
+    () => (random == 0 ? BIOMARKERS : BIOMARKERS.sort(() => random - 0.5)),
+    [random],
+  );
+  const list1 = useMemo(() => shuffledList.slice(0, 6), [shuffledList]);
+  const list2 = useMemo(
+    () => shuffledList.slice(6, shuffledList.length),
+    [shuffledList],
+  );
+  console.log(random);
+  console.log(list1);
   const listCombined = [list1, list2];
   const inViewDelay = 0.6;
+
+  const simplex = useMemo(() => new SimplexNoise(), []);
 
   return (
     <div className="flex-column relative flex flex-col gap-2 py-16">
@@ -53,10 +68,17 @@ const BiomarkerBanner = ({ isVisible = true }: Props) => {
             }}
           >
             {list.map((item, innerIndex) => {
+              const randomValue = Math.floor(
+                Math.abs(
+                  simplex.noise(random * index * innerIndex, 0) * colors.length,
+                ),
+              );
+
+              console.log(randomValue);
+
               const randomColor =
-                innerIndex % 2 === 0
-                  ? colors[Math.floor(Math.random() * colors.length)]
-                  : "#000000";
+                innerIndex % 2 === 0 ? colors[randomValue] : "#000000";
+
               return (
                 <div
                   key={innerIndex}
