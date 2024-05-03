7b4800ff3e6cff070025e7db9865c0d0b40f0716
Alvin Leung <alvinleung2009@gmail.com>
2024-05-03 13:55:54 -0700

    force scroll to top when page load

--- components/LoadingScreen/LoadingScreen.tsx
+++ components/LoadingScreen/LoadingScreen.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import { motion } from "framer-motion";
-import React, { useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 
 type Props = {
   isLoaded: boolean;
 };
 
 const LoadingScreen = ({ isLoaded = false }: Props) => {
+  useLayoutEffect(() => {
+    if (isLoaded) return;
+    // reset to zero
+    window.scrollTo(0, 0);
+
+    // prevent user scroll when the page is not loaded
+    const cleanup = disableScroll();
+    return () => cleanup();
+  }, [isLoaded]);
   return (
     <motion.div
       className="fixed inset-0 z-[9999999] bg-zinc-900"
@@ -22,3 +31,38 @@ const LoadingScreen = ({ isLoaded = false }: Props) => {
 };
 
 export default LoadingScreen;
+
+// call this to Disable
+function disableScroll() {
+  // left: 37, up: 38, right: 39, down: 40,
+  // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+  var keys: any = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+  function preventDefault(e: Event) {
+    e.preventDefault();
+  }
+
+  function preventDefaultForScrollKeys(e: KeyboardEvent) {
+    if (keys[e.keyCode]) {
+      preventDefault(e);
+      return false;
+    }
+  }
+
+  var wheelEvent =
+    "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
+
+  // window.addEventListener("DOMMouseScroll", preventDefault, false); // older FF
+  window.addEventListener("wheel", preventDefault, { passive: false }); // modern desktop
+  window.addEventListener("touchmove", preventDefault, { passive: false }); // mobile
+  window.addEventListener("keydown", preventDefaultForScrollKeys, false);
+
+  return () => {
+    // window.removeEventListener("DOMMouseScroll", preventDefault, false); // older FF
+    //@ts-ignore
+    window.removeEventListener("wheel", preventDefault, { passive: false }); // modern desktop
+    //@ts-ignore
+    window.removeEventListener("touchmove", preventDefault, { passive: false }); // mobile
+    window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
+  };
+}
